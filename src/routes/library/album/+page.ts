e1a78616f2cab3cc17770b2ff61d9a5ccb08f46b
wxllow <willow@wxllow.dev>
2024-02-18 14:51:50 -0500

    [fix] dont run /lib/album onnon  browser

--- src/routes/library/album/+page.ts
+++ src/routes/library/album/+page.ts
@@ -1,8 +1,10 @@
 import { error } from '@sveltejs/kit';
-
+import { browser } from '$app/environment';
 import type { Album } from '$lib/db';
 
 export async function load({ url }) {
+    if (!browser) return;
+
     const slug = url.searchParams.get('id');
 
     if (!slug) {
