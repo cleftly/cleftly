62d3f9dfd927920090933bae5adb21b9b5d843e3
wxllow <willow@wxllow.dev>
2024-02-18 14:41:25 -0500

    [fix] /library/albums only when browser

--- src/routes/library/albums/+page.ts
+++ src/routes/library/albums/+page.ts
@@ -1,4 +1,8 @@
+import { browser } from '$app/environment';
+
 export async function load() {
+    if (!browser) return;
+
     const { default: db } = await import('$lib/db');
 
     const albums = await Promise.all(
